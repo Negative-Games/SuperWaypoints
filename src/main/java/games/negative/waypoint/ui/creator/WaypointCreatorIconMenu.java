f6b2ff1c0e53865097c7a93c796b0b32da45ec2e
Eric <negggative@gmail.com>
2023-05-01 19:14:59 -0300

    Comment out bad code

--- src/main/java/games/negative/waypoint/ui/creator/WaypointCreatorIconMenu.java
+++ src/main/java/games/negative/waypoint/ui/creator/WaypointCreatorIconMenu.java
@@ -24,6 +24,8 @@ public class WaypointCreatorIconMenu extends GUI {
         Material[] values = Material.values();
         int limit = (6 * 9) - values.length;
 
+        /*
+        sending stacktrace in console, will fix later
         Arrays.stream(values).filter(material -> !material.name().contains("AIR") && (material.isItem() || material.isBlock())).skip((long) (page - 1) * limit).limit(limit).forEach(material -> {
             ItemStack item = new ItemStack(material);
 
@@ -32,6 +34,8 @@ public class WaypointCreatorIconMenu extends GUI {
                 new WaypointCreatorMenu(manager, builder).open(player);
             });
         });
+
+         */
 
     }
 }
