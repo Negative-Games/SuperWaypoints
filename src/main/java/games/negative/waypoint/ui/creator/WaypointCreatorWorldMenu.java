c77c07fac677c453db6dce70df139ac670be28a5
Negative <negggative@gmail.com>
2023-05-02 05:23:32 -0300

    Fix filler positions in world selector

--- src/main/java/games/negative/waypoint/ui/creator/WaypointCreatorWorldMenu.java
+++ src/main/java/games/negative/waypoint/ui/creator/WaypointCreatorWorldMenu.java
@@ -20,7 +20,7 @@ public class WaypointCreatorWorldMenu extends GUI {
 
         List<Integer> fillerSlots = Lists.newArrayList(
                 0, 1, 2, 3, 4, 5, 6, 7, 8,
-                9, 17, 18, 26, 27, 35, 36, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53
+                9, 17, 18, 26, 27, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44
         );
         ItemStack blackFillerItem = Item.BLACK_FILLER.getItem();
 
